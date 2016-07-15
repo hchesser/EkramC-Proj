0476050688045252a9c1923a84b887aae756b0ef
Ekram Bhuiyan <ekram67@my.yorku.ca>
2016-07-15 17:51:14 -0400

    Add files via upload

---
+++ async.h
@@ -0,0 +1,30 @@
+#ifndef _ASYNC_H
+#define _ASYNC_H
+
+/* Includes */
+#include <termios.h> /* POSIX terminal control definitions */
+#include <stdio.h>
+#include <unistd.h> /* UNIX standard function definitions */
+#include <fcntl.h>  /* File control definitions */
+#include <sys/signal.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+/* Macros */
+ #define BAUDRATE B115200
+ #define DEVICE "/dev/ttyS2"
+ #define _POSIX_SOURCE 1 /* POSIX compliant source */
+ #define FALSE 0
+ #define TRUE 1
+ #define BUF_SIZE 1024 /* MTU of a KISS packet */
+
+/*Data*/
+int fd;
+
+/* Prototypes */
+void signal_handler_IO (int signo, siginfo_t *siginfo, void *ucontext);   /* definition of a signal handler for SIGIO */
+void signal_handler_SEG (int signo, siginfo_t *siginfo, void *ucontext); /* definition  of a signal handler for SIGSEG */  
+void signal_handler_INT (int signo, siginfo_t *siginfo, void *ucontext); /* definition of signal handler */
+#endif
